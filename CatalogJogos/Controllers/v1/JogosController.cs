982b14032448faca4e1c4480f25d933dc294ac54
Moacir Afonso <36249463+moacir9401@users.noreply.github.com>
2021-10-22 22:02:57 -0300

    criando controller e models e services

---
+++ CatalogJogos/Controllers/v1/JogosController.cs
@@ -0,0 +1,121 @@
+﻿using CatalogJogos.InputModel;
+using CatalogJogos.Services;
+using CatalogJogos.ViewModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogJogos.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class JogosController : ControllerBase
+    {
+        private readonly IJogoService _jogoService;
+
+        public JogosController(IJogoService service)
+        {
+            _jogoService = service;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<JogoViewModel>>> Obter([FromQuery, Range(1, int.MaxValue)] int pagina = 1, [FromQuery, Range(1, 50)] int quantidade = 5)
+        {
+            var jogos = await _jogoService.Obter(pagina, quantidade);
+
+            if (!jogos.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(jogos);
+        }
+
+        [HttpGet("{idJogo: guild}")]
+        public async Task<ActionResult<JogoViewModel>> Obter([FromRoute] Guid idJogo)
+        {
+            var jogos = await _jogoService.Obter(idJogo);
+
+            if (jogos == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(jogos);
+        }
+
+        [HttpPost()]
+        public async Task<ActionResult<JogoViewModel>> Inserir([FromBody] JogoInputModel jogoInputModel)
+        {
+            try
+            {
+                var jogo = await _jogoService.Inserir(jogoInputModel);
+
+                return Ok(jogo);
+            }
+            catch (Exception ex)
+            {
+                return UnprocessableEntity("Já existe um jogo com este nome para esta produtora");
+            }
+        }
+
+        [HttpPut("{idJogo: guild}")]
+        public async Task<ActionResult> AtualizarJogo([FromQuery] Guid idJogo, [FromBody] JogoInputModel jogoInputModel)
+        {
+
+            try
+            {
+                await _jogoService.Atualizar(idJogo, jogoInputModel);
+
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return UnprocessableEntity("Não existe esse jogo");
+            }
+        }
+
+        [HttpPatch("{idJogo: guild}/preco/{preco:double}")]
+        public async Task<ActionResult> AtualizarJogo([FromQuery] Guid idJogo, [FromQuery] double preco)
+        {
+            try
+            {
+                try
+                {
+                    await _jogoService.Atualizar(idJogo, preco);
+
+                    return Ok();
+                }
+                catch (Exception)
+                {
+                    return UnprocessableEntity("Não existe esse jogo");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete("{idJogo: guild}")]
+        public async Task<ActionResult> ApagarJogo([FromQuery] Guid idJogo)
+        {
+            try
+            {
+                await _jogoService.Remover(idJogo);
+                return Ok();
+            }
+            catch (Exception)
+            {
+
+                return UnprocessableEntity("Não existe esse jogo");
+            }
+        }
+
+    }
+}
---
+++ CatalogJogos/Services/IJogoService.cs
@@ -0,0 +1,19 @@
+﻿using CatalogJogos.InputModel;
+using CatalogJogos.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogJogos.Services
+{
+    public interface IJogoService
+    {
+        Task<List<JogoViewModel>> Obter(int pagina, int quantidade);
+        Task<JogoViewModel> Obter(Guid idJogo);
+        Task<JogoViewModel> Inserir(JogoInputModel jogo);
+        Task Atualizar(Guid idJogo, JogoInputModel jogo);
+        Task Atualizar(Guid idJogo, double preco);
+        Task Remover(Guid idJogo);
+    }
+}
